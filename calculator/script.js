cd8ae895c5beb4d8a67c2884d592a92ef7c7c3b6
l3aobab <noelonazo@gmail.com>
2022-03-10 20:11:47 +0100

    OOP Calculator

--- calculator/script.js
+++ calculator/script.js
@@ -65,12 +65,12 @@ class Calculator {
 		const integerDigits = parseFloat(stringNumber.split('.')[0])
 		const decimalDigits = stringNumber.split('.')[1]
 		let integerDisplay
-		if (isNaN(integerDisplay)) {
+		if (isNaN(integerDigits)) {
 			integerDisplay = ''
 		} else {
 			integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
 		}
-		
+	
 		if (decimalDigits != null) {
 			return `${integerDisplay}.${decimalDigits}`
 		} else {
@@ -78,11 +78,12 @@ class Calculator {
 		}
 	}
 
-
 	updateDisplay() {
-		this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+		this.currentOperandTextElement.innerText = 
+			this.getDisplayNumber(this.currentOperand)
 		if (this.operation != null) {
-			this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+			this.previousOperandTextElement.innerText = 
+				`${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
 		} else {
 			this.previousOperandTextElement.innerText = ''
 		}
