7aaee543fd41794059576d059a5813061d46e75b
l3aobab <noelonazo@gmail.com>
2022-05-02 12:36:40 +0200

    Simple Birds

--- birds/script.js
+++ birds/script.js
@@ -1,4 +1,5 @@
 import { updateBird, setupBird, getBirdRect } from "./bird.js"
+import { updatePipe, setupPipes, getPassedPipesCount, getPipeRects } from "./pipe.js"
 
 document.addEventListener("keypress", handleStart, { once: true })
 const title = document.querySelector("[data-title]")
@@ -14,6 +15,7 @@ function updateLoop(time) {
 	}
 	const delta = time - lastTime
 	updateBird(delta)
+	updatePipe(delta)
 	if (checkLose()) return handleLose()
 	lastTime = time
 	window.requestAnimationFrame(updateLoop)
@@ -21,13 +23,24 @@ function updateLoop(time) {
 
 function checkLose() {
 	const birdRect = getBirdRect()
+	const insidePipe = getPipeRects().some(rect => isCollision(birdRect, rect))
 	const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
-	return outsideWorld
+	return outsideWorld || insidePipe
+}
+
+function isCollision(rect1, rect2) {
+	return (
+		rect1.left < rect2.right &&
+		rect1.top < rect2.bottom &&
+		rect1.right > rect2.left &&
+		rect1.bottom > rect2.top
+	)
 }
 
 function handleStart() {
 	title.classList.add("hide")
 	setupBird()
+	setupPipes()
 	lastTime = null
 	window.requestAnimationFrame(updateLoop)
 }
@@ -36,7 +49,7 @@ function handleLose() {
 	setTimeout(() => {
 		title.classList.remove("hide")
 		subtitle.classList.remove("hide")
-		subtitle.textContent = "0 Pipes"
+		subtitle.textContent = `${getPassedPipesCount()} Pipes`
 		document.addEventListener("keypress", handleStart, { once: true })
 	}, 100)
 
