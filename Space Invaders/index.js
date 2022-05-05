077b1bd44c62646f9436366283a6435e6eb970fb
l3aobab <noelonazo@gmail.com>
2022-05-05 11:58:35 +0200

    Space Invaders - player movement
    Player movement

--- Space Invaders/index.js
+++ Space Invaders/index.js
@@ -12,6 +12,8 @@ class Player {
 			y: 0
 		}
 
+		this.rotation = 0
+
 		const image = new Image()
 		image.src = './img/spaceship.png'
 		image.onload = () => {
@@ -34,26 +36,93 @@ class Player {
 		//	this.width, 
 		//	this.height
 		//)
+
+		context.save()
+		context.translate(player.position.x + player.width / 2, player.position.y + player.height / 2)
+		context.rotate(this.rotation)
+		
+		context.translate(-player.position.x - player.width / 2, -player.position.y - player.height / 2)
+
+		context.drawImage(
+			this.image, 
+			this.position.x, 
+			this.position.y,
+			this.width,
+			this.height
+		)
+
+		context.restore()
+	}
+
+	update() {
 		if (this.image) {
-			context.drawImage(
-				this.image, 
-				this.position.x, 
-				this.position.y,
-				this.width,
-				this.height
-			)
+			this.draw()
+			this.position.x += this.velocity.x
 		}
 	}
 }
 
 const player = new Player()
-player.draw()
+const keys = {
+	a: {
+		pressed: false
+	},
+	d: {
+		pressed: false
+	},
+	space: {
+		pressed: false
+	}
+}
 
 function animate() {
 	requestAnimationFrame(animate)
 	context.fillStyle = 'black'
 	context.fillRect(0, 0, canvas.width, canvas.height)
-	player.draw()
+	player.update()
+
+	if (keys.a.pressed && player.position.x >= 0) {
+		player.velocity.x = -7
+		player.rotation = -0.15
+	} else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+		player.velocity.x = 7
+		player.rotation = 0.15
+	}else {
+		player.velocity.x = 0
+		player.rotation = 0
+	}
 }
 
 animate()
+
+addEventListener('keydown', ({ key }) => {
+	switch (key) {
+		case 'a':
+			console.log('left')
+			keys.a.pressed = true
+			break
+		case 'd':
+			console.log('right')
+			keys.d.pressed = true
+			break
+		case ' ':
+			console.log('space')
+			break
+	}
+})
+
+addEventListener('keyup', ({ key }) => {
+	switch (key) {
+		case 'a':
+			console.log('left')
+			keys.a.pressed = false
+			break
+		case 'd':
+			console.log('right')
+			keys.d.pressed = false
+			break
+		case ' ':
+			console.log('space')
+			break
+	}
+})
